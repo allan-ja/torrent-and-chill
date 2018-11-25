8576e61a9ab806761331491483b3bcd11eff6021
Allan J <sc15aj@leeds.ac.uk>
2018-11-25 14:57:46 +0100

    Redux + fetchMovies added

--- client/src/components/ContentGridList.js
+++ client/src/components/ContentGridList.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 import StarRate from '@material-ui/icons/StarRate'
 import { GridList, GridListTile, GridListTileBar } from '@material-ui/core';
+
+import { fetchMovies } from '../actions'
 
 const styles = theme => ({
     root: {
@@ -19,29 +21,35 @@ const styles = theme => ({
     }
 })
 
-const movies = [{"_id":"tt3606756","imdb_id":"tt3606756","title":"Incredibles 2","year":"2018","synopsis":"Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.","runtime":"118","released":1529020800,"certification":"PG","torrents":{"en":{"1080p":{"provider":"YTS","filesize":"1.87 GB","size":2007897211,"peer":7138,"seed":18930,"url":"magnet:?xt=urn:btih:6B7469C830B624321F843A95D0BD162AAF2ABFF4&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337"},"720p":{"provider":"YTS","filesize":"992.65 MB","size":1040868966,"peer":7739,"seed":14301,"url":"magnet:?xt=urn:btih:23FCDD2D194D479DE2C38E1DD1BEFE6D8B28AF9B&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337"}}},"trailer":"http://youtube.com/watch?v=i5qOzqD9Rms","genres":["animation","family","action","adventure","superhero"],"images":{"poster":"http://image.tmdb.org/t/p/w500/x1txcDXkcM65gl7w20PwYSxAYah.jpg","fanart":"http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg","banner":"http://image.tmdb.org/t/p/w500/x1txcDXkcM65gl7w20PwYSxAYah.jpg"},"rating":{"percentage":81,"watching":110,"votes":8648,"loved":100,"hated":100}}]
 
-function MovieCard(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-        <GridList cellHeight={180} className={classes.gridList}>
-            {movies.map(tile => (
-                <GridListTile key={tile._id}>
-                    <img src={tile.images.banner} />
-                    <GridListTileBar
-                        title={tile.title}
-                    />
-                </GridListTile>
-            ))}
-        </GridList>
+class ContentGridList extends Component {
+    componentDidMount() {
+        this.props.fetchMovies()
+    }
 
-    </div>
-  );
+    render() {
+        const { classes } = this.props
+        return (
+            <div className={classes.root}>
+                <GridList cellHeight={180} className={classes.gridList}>
+                    {this.props.movies.map(tile => (
+                        <GridListTile key={tile._id}>
+                            <img src={tile.images.banner} />
+                            <GridListTileBar
+                                title={tile.title}
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+        )
+    }
 }
 
-MovieCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+function mapStateToProps({ movies }) {
+    return { movies }
+}
 
-export default withStyles(styles)(MovieCard);
+export default connect(
+    mapStateToProps, { fetchMovies })(
+        withStyles(styles)(ContentGridList))
